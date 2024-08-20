790bcaee630759448186bce54534c522b8163bfe
Trevor Johnson <trevor9j@protonmail.com>
2024-08-20 11:16:01 -0700

    added button to update/set user's name in local storage, added prompt to ask for user's name if a name isnt saved to local storage.

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,30 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "/images/firefox-icon.png");
   }
 }
+
+/*Selecting the html button and h1 tag */
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+/* Function to set user's name in local storage and update the heading. This function first checks if theres no "name" value saved in local storage. If there isnt, the prompt wont close until a name is entered */
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+  localStorage.setItem("name", myName);
+  myHeading.textContent =`Mozilla is cool, ${myName}`;
+  }
+}
+
+/* initialization code, as it structures the app when it first loads. */
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
